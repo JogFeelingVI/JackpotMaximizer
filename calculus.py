9038b18aaf75d829c0272b2168be473bcf34706a
JogfeeingVI <lifelse@outlook.com>
2023-10-28 06:15:36 +0800

    update

--- calculus.py
+++ calculus.py
@@ -2,7 +2,7 @@
 # @Author: JogFeelingVi
 # @Date: 2022-10-03 15:26:39
 # @Last Modified by:   JogFeelingVI
-# @Last Modified time: 2023-10-27 20:12:32
+# @Last Modified time: 2023-10-27 22:49:03
 from re import T
 from codex import funcs
 from codex.runingtime import runingtime
