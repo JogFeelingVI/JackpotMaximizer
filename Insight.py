2670f4d0a3b044c3f81a8f1d5dc72b41afd86387
jogfeelingvi <lifelse@outlook.com>
2024-01-10 17:08:27 +0800

    Update Insight.py

--- Insight.py
+++ Insight.py
@@ -2,7 +2,7 @@
 # @Author: Your name
 # @Date:   2024-01-07 14:18:41
 # @Last Modified by:   Your name
-# @Last Modified time: 2024-01-10 16:57:33
+# @Last Modified time: 2024-01-10 17:07:29
 
 import itertools, re, time
 from codex import Tonji
@@ -16,18 +16,21 @@ def main():
         list_of_lists = [tjone.parse_fps(line=line) for line in f]
         for lol in list_of_lists:
             tjone.add(lol)
-    whereis = tjone.where_is(key=lambda x: len(x[1]), value=1, operator='==')
+    whereis = tjone.where_is(key=lambda x: len(x[1]), value=3, operator='==')
 
     filter_dict = {
         k: v
         for k, v in filter(whereis, tjone.same_numbers_dict.items())
     }
     grouped_dict = itertools.groupby(filter_dict.items(), lambda x: x[1])
+    group_count = 0
     for key, group in grouped_dict:
         # key 是分组的 key，group 是分组元素的迭代器
         _t = list(group)
         if len(_t) == 6:
             print(f'Key: {key}, Values: {_t}')
+            group_count +=1
+    print(f'group len {group_count}')
 
 
 if __name__ == "__main__":
