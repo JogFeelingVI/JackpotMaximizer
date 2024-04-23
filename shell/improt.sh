81d77250d210d70a08cd7784df7ed543f2c450d7
jogfeelingvi <lifelse@outlook.com>
2024-04-23 10:19:30 +0800

    update

--- shell/improt.sh
+++ shell/improt.sh
@@ -2,13 +2,11 @@
 # @Author: JogFeelingVI
 # @Date:   2024-04-21 21:05:21
 # @Last Modified by:   JogFeelingVI
-# @Last Modified time: 2024-04-21 21:45:02
-
-#!/bin/bash
+# @Last Modified time: 2024-04-23 10:17:41
 
 # 定义源文件夹和目标文件夹
-source_dir=~/Github/autoasset
-target_dir=~/Github/JackpotMaximizer
+source_dir=~/Downloads/Github/autoasset
+target_dir=~/Downloads/Github/JackpotMaximizer
 
 # 复制文件
 dataf="DataFrame.json"
