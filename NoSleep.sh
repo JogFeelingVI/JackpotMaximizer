e0b781e42c9a73ff61972ff3a58fb0f4502cbd66
JogfeeingVI <lifelse@outlook.com>
2024-09-22 12:10:10 +0800

    update

--- NoSleep.sh
+++ NoSleep.sh
@@ -2,7 +2,7 @@
 # @Author: Your name
 # @Date:   2024-09-17 07:10:25
 # @Last Modified by:   Your name
-# @Last Modified time: 2024-09-19 06:58:37
+# @Last Modified time: 2024-09-22 07:58:32
 start_time=$(date +%s)
 # 设置 Python 解释器路径
 PYTHON_PATH="./.venv/bin/python"  # 或你的 Python 解释器路径
@@ -15,6 +15,6 @@ sudo systemd-inhibit --what=sleep --who="Perform_exploration" --why="Running pyt
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 
-ps -ef | grep './venv'| grep -v 'pts/0' | awk '{print $2}' | xargs kill -9
+ps -ef | grep './venv'| grep -v 'pts' | awk '{print $2}' | xargs kill -9
 # 可选: 打印程序返回值
 echo "Execution time: $execution_time seconds"
